57eda4d058bd242cc1308628fcd6f4cb1ce8a269
ShonSagoro <ramosproque@gmail.com>
2024-07-15 22:13:56 -0600

    fix Dockerfile

--- src/middleware/RateLimiter.ts
+++ src/middleware/RateLimiter.ts
@@ -1,11 +1,12 @@
 import rateLimit from 'express-rate-limit';
 import { BaseResponse } from './dtos/BaseResponse';
+import { Request, Response } from 'express';
 
 export const rateLimiter = rateLimit({
     windowMs: 1000,
     max: 10,
     headers: true,
-    handler: (req, res) => {
+    handler: (req: Request, res: Response) => {
         const baseResponse = new BaseResponse(null, 'Too many requests from this IP, please try again after 5 seconds.', false, 429);
         baseResponse.apply(res);
     },
